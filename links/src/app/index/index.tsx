c28e2cbf4032e54d47c42bed341651e0ca9e5c3e
danielfiorotefernandes <danielfiorote@hotmail.com>
2024-12-17 13:22:04 -0300

    teste

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -1,13 +1,25 @@
-import { Image, View } from "react-native";
+import { Image, View, TouchableOpacity } from "react-native";
 
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { colors } from "@/styles/colors";
 import { s } from "./styles";
+
+//components
+
+import { Category } from "@/components/category";
 
 export default function Index() {
   return (
     <View style={s.container}>
       <View style={s.header}>
         <Image source={require("@/assets/logo.png")} style={s.logo} />
+
+        <TouchableOpacity>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
       </View>
+      <Category />
     </View>
   );
 }
