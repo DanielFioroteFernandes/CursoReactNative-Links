ecc1bea0e2defc1f8bd2fda030e539849c72f561
danielfiorotefernandes <danielfiorote@hotmail.com>
2024-12-18 21:28:59 -0300

    final modulo

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -1,4 +1,11 @@
-import { Image, View, TouchableOpacity } from "react-native";
+import {
+  Image,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Text,
+} from "react-native";
 
 import { MaterialIcons } from "@expo/vector-icons";
 
@@ -9,6 +16,7 @@ import { s } from "./styles";
 
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
+import { Option } from "@/components/option";
 
 export default function Index() {
   return (
@@ -22,11 +30,43 @@ export default function Index() {
       </View>
       <Categories />
 
-      <Link
-        name="RockFiorote"
-        url="http://teste.fdp.com"
-        onDetails={() => console.log("clicou")}
+      <FlatList
+        data={["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]}
+        keyExtractor={(item) => item}
+        renderItem={() => (
+          <Link
+            name="RockFiorote"
+            url="http://teste.fdp.com"
+            onDetails={() => console.log("clicou")}
+          />
+        )}
+        style={s.links}
+        contentContainerStyle={s.linksContent}
+        showsVerticalScrollIndicator={false}
       />
+
+      <Modal transparent visible={false}>
+        <View style={s.modal}>
+          <View style={s.modalContent}>
+            <View style={s.modalHeader}>
+              <Text style={s.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text style={s.modalLinkName}>Rocktseat</Text>
+            <Text style={s.modalUrl}>http://teste.fdp.com</Text>
+            <View style={s.modalFooter}>
+              <Option name="Excluir" icon="delete" variant="secondary" />
+              <Option name="Abrir" icon="language" />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
